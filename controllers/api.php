355c0abd5180f0d3e4c0edadaf2124e56d3db516
Martin <mv1u18@soton.ac.uk>
2020-11-23 23:21:48 +0000

    API no longer exposed with empty token param

--- controllers/api.php
+++ controllers/api.php
@@ -6,9 +6,18 @@ class Api extends Controller {
 		extract($f3->get('PARAMS'));
 		extract($f3->get('GET'));
 
-		//Check for authentication token and fail without
-		if(!isset($token) || $token != $f3->get('site.apikey')) {
-			echo json_encode(array('error' => '403')); die();
+		// Check for authentication token and fail without
+		// !isset($var) returns false when there is a GET param called token, but is empty,
+		// meaning an empty token could bypass this authorisation check
+		// $token == NULL is a more comprehensive check, which returns false only when
+		// variable token is set and is non-empty
+		if($token == NULL || $token != $f3->get('apikey')) {
+			// Deny access when DEBUG mode is off (and token matches apikey)
+			if($this->db->connection->
+			exec("SELECT `value` from `settings` where `setting`='debug'")[0]['value'] == "0"){
+				echo json_encode(array('error' => '403'));
+				die();
+			}
 		}
 
 		//Provide API access
