ae54e101ebe37b3edf96f12a5372abebd8d35a18
Martin <mv1u18@soton.ac.uk>
2020-12-06 04:38:09 +0000

    fix mail vulns (except param validation)

--- controllers/contact.php
+++ controllers/contact.php
@@ -4,10 +4,11 @@ class Contact extends Controller {
 
 	public function index($f3) {
 		if($this->request->is('post')) {
-			extract($this->request->data);
-			$from = "From: $from";
-			//TODO: fix mail paramater validation
-			mail($to,$subject,$message,$from);
+			$data = $this->request->data;
+			$email = $this->Auth->user('email');
+			$from = 'From: '.($email ? $email : 'Unregistered ('.$data['from'].')');
+			$to = $f3->get('site.email');
+			mail($to,$data['subject'],$data['message'],$from);
 
 			StatusMessage::add('Thank you for contacting us');
 			return $f3->reroute('/');
