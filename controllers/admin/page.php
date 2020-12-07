05448a8b8ca8e3384d9050b611901ec6e9a3044a
Martin <mv1u18@soton.ac.uk>
2020-12-07 01:05:52 +0000

    parameter validation for page title

--- controllers/admin/page.php
+++ controllers/admin/page.php
@@ -2,6 +2,8 @@
 
 namespace Admin;
 
+use Respect\Validation\Validator as v;
+
 class Page extends AdminController {
 
 	public function index($f3) {
@@ -12,18 +14,24 @@ class Page extends AdminController {
 	public function add($f3) {
 		if($this->request->is('post')) {
 			$pagename = strtolower(str_replace(" ","_",$this->request->data['title']));
-			$this->Model->Pages->create($pagename);
-		
-			\StatusMessage::add('Page created successfully','success');
-			return $f3->reroute('/admin/page/edit/' . $pagename);
+			try {
+				v::StringType()->notOptional()->not(v::startsWith(' '))
+					->check($this->request->data['title']);
+				$this->Model->Pages->create($pagename);
+				\StatusMessage::add('Page created successfully','success');
+				return $f3->reroute('/admin/page/edit/' . $pagename);
+			} catch(\Exception $exception) {
+				\StatusMessage::add($exception->getMessage(),'danger');
+				return $f3->reroute('/admin/page');
+			}	
 		}
 	}
 
 	public function edit($f3) {
 		$pagename = $f3->get('PARAMS.3');
 		// return early if page doesn't exist
-		$page = $this->Model->Pages->fetch($pagename);
-		if(!$page) return $f3->reroute('/admin/page');
+		if(!array_key_exists(strtolower($pagename), array_change_key_case($this->Model->Pages->fetchAll())))
+			return $f3->reroute('/admin/page');
 		if ($this->request->is('post')) {
 			$pages = $this->Model->Pages;
 			$pages->title = $pagename;
@@ -35,7 +43,7 @@ class Page extends AdminController {
 		}
 	
 		$pagetitle = ucfirst(str_replace("_"," ",str_ireplace(".html","",$pagename)));	
-		//$page = $this->Model->Pages->fetch($pagename);
+		$page = $this->Model->Pages->fetch($pagename);
 		$f3->set('pagetitle',$pagetitle);
 		$f3->set('page',$page);
 	}
--- controllers/page.php
+++ controllers/page.php
@@ -4,9 +4,10 @@ class Page extends Controller {
 
 	function display($f3) {
 		$pagename = urldecode($f3->get('PARAMS.3'));
+		// return early if page doesn't exist
+		if(!array_key_exists(strtolower($pagename), array_change_key_case($this->Model->Pages->fetchAll())))
+			return $f3->error(404);
 		$page = $this->Model->Pages->fetch($pagename);
-		//if page doesn't exist return instead of displaying blank page
-		if(!$page) return $f3->error(404);
 		$pagetitle = ucfirst(str_replace("_"," ",str_replace(".html","",$pagename)));
 		$f3->set('pagetitle',$pagetitle);
 		$f3->set('page',$page);
